835f34244429c0d382300726ce23fa1b083723ef
Dexter <denus.denisovuch@gmail.com>
2020-07-29 23:08:36 +0300

    Version 0.6

--- js/main.js
+++ js/main.js
@@ -1,34 +1,37 @@
-$('.feature-carousel').owlCarousel({
-    pagination : true,
-    loop: true,
-    margin: 30,
-    center: true,
-    dots:true,
-    autoplay: true,
-    autoplayTimeout: 4000,
-    responsive: {
-        0: {
-            items: 1,
-            
-        },
-        575: {
-            items: 1,
-            
-        },
-        768: {
-            items: 2
-        },
-        991: {
-            items: 2
-        },  
-        992: {
-            items: 3
-        }
-    }
-})
-
-$('.counter').counterUp({
-    delay: 10,
-    time: 1000
+$(".feature-carousel").owlCarousel({
+  pagination: true,
+  loop: true,
+  margin: 30,
+  center: true,
+  dots: true,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    575: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    991: {
+      items: 2,
+    },
+    992: {
+      items: 3,
+    },
+  },
 });
 
+$(".counter").counterUp({
+  delay: 10,
+  time: 1000,
+});
+
+var t = $(".video-btn");
+t.length &&
+  t.magnificPopup({
+    type: "iframe",
+  });
