da68d7bc1cc96b8057ac64c5e040f76368018a74
Dexter <denus.denisovuch@gmail.com>
2020-07-29 18:12:12 +0300

    version 0.2

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$('.feature-carousel').owlCarousel({
+    pagination : true,
+    loop: true,
+    margin: 30,
+    center: true,
+    dots:true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    responsive: {
+        0: {
+            items: 1,
+            
+        },
+        575: {
+            items: 1,
+            
+        },
+        768: {
+            items: 2
+        },
+        991: {
+            items: 2
+        },  
+        992: {
+            items: 3
+        }
+    }
+})
+
