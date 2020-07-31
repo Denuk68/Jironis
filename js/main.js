33cbffb3d4f936a7a1e8d54b03d706ecc9886ed4
Dexter <denus.denisovuch@gmail.com>
2020-07-31 21:56:10 +0300

    Version 1.0

--- js/main.js
+++ js/main.js
@@ -63,3 +63,87 @@ $('.app-carousel').owlCarousel({
       }
   }
 })
+
+
+
+
+// Author Carousel
+let author = $('.author-carousel')
+author.owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    center:true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        500: {
+            items: 1
+        },
+        768: {
+            items: 1
+        },
+        992: {
+            items: 1
+        }
+    }
+})
+
+let author_cumment = $('.author-comment-carousel');
+author_cumment.owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    navText: ['<i class="fa fa-angle-left">', '<i class="fa fa-angle-right">'],
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        500: {
+            items: 1
+        },
+        768: {
+            items: 1
+        },
+        992: {
+            items: 1
+        }
+    }
+});
+
+   $('.author-comment-carousel .owl-next').on('click', function () {
+       $('.author-carousel .owl-next').click()
+   });
+
+   $('.author-comment-carousel .owl-prev').on('click', function () {
+       $('.author-carousel .owl-prev').click()
+   });
+
+
+  //  Pzerner-carousel
+   $('.partner-carousel').owlCarousel({
+            loop: true,
+            margin: 20,
+            nav: false,
+            dots: false,
+            autoplay:true,
+            autoplayTimeout:4000,
+            autoplaySpeed:3000,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                500: {
+                    items: 3
+                },
+                768: {
+                    items: 4
+                },
+                992: {
+                    items: 5
+                }
+            }
+        })
