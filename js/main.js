d69240934fc9d73a029c06c78fd85680aa885727
Dexter <denus.denisovuch@gmail.com>
2020-08-03 00:03:29 +0300

    Version 1.3

--- js/main.js
+++ js/main.js
@@ -153,4 +153,22 @@ $(".partner-carousel").owlCarousel({
   },
 });
 
+// Modal button
+$(document).ready(function() {
+  $('button#myLinkModal').click( function(event){
+    event.preventDefault();
+    $('#myOverlay').fadeIn(297,	function(){
+      $('#myModal') 
+      .css('display', 'block')
+      .animate({opacity: 1}, 198);
+    });
+  });
+
+  $('#myModal__close, #myOverlay').click( function(){
+    $('#myModal').animate({opacity: 0}, 198, function(){
+      $(this).css('display', 'none');
+      $('#myOverlay').fadeOut(297);
+    });
+  });
+});
 
