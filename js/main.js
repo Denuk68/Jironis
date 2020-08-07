74536f8f88a491ba45cdf19abda9a0f4c300081a
Dexter <denus.denisovuch@gmail.com>
2020-08-08 00:29:35 +0300

    Version 1.9

--- js/main.js
+++ js/main.js
@@ -170,12 +170,84 @@ $(document).ready(function () {
   });
 });
 
-
-
-
 // Mask
-$("#inputPhone").inputmask({"mask": "+380 (99) 99-99-999"});
+$("#inputPhone").inputmask({ mask: "+380 (99) 99-99-999" });
 
+//Form
+function validateName() {
+  let input = document.querySelector(".form-group #inputName");
+  let name = document.forms["app-form"]["name"].value;
+  let pattern = /^[a-zA-Zа-яА-Я]+(([',. -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
+  if (name == "") {
+    input.style.border = "2px solid red";
+    input.placeholder = "Поле обов'язкове";
+    console.log("null");
+  } else if (!name.match(pattern)) {
+    input.style.border = "2px solid red";
+    input.placeholder = "Некоректно введені дані";
+    console.log("incorrect");
+  } else if (name.length < 3) {
+    input.style.border = "2px solid red";
+    input.placeholder = "Мінімум 3 символа";
+    console.log("min 3 symbol");
+    return false;
+  } else if (name.length > 25) {
+    input.style.border = "2px solid red";
+    input.placeholder = "Максимум 25 символів";
+    console.log("max 25 sym");
+    return false;
+  } else {
+    input.style.border = "2px solid green";
+    console.log("true");
+    return true;
+  }
+}
+
+function validatePhone() {
+  let input = document.querySelector(".form-group #inputPhone");
+  let phone = document.forms["app-form"]["phone"].value;
+  let pattern = /^[+]*3?8?0?[\s][(]{1}[0-9]{2}[)]{1}[\s][0-9]{2}[-][0-9]{1,3}[-][0-9]{3}/;
+  if (phone == "") {
+    input.style.border = "2px solid red";
+    input.placeholder = "Поле обов'язкове";
+    console.log("null");
+  } else if (!phone.match(pattern)) {
+    input.style.border = "2px solid red";
+    input.placeholder = "Некоректно введені дані";
+    console.log("incorrect");
+  } else {
+    input.style.border = "2px solid green";
+    console.log("true");
+    return true;
+  }
+}
+
+function validateEmail() {
+  let input = document.querySelector(".form-group #inputEmail");
+  let email = document.forms["app-form"]["email"].value;
+  let pattern = /^^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  if (email == "") {
+    input.style.border = "2px solid red";
+    input.placeholder = "Поле обов'язкове";
+    console.log("null");
+  } else if (!email.match(pattern)) {
+    input.style.border = "2px solid red";
+    input.placeholder = "Некоректно введені дані";
+    console.log("incorrect");
+  } else {
+    input.style.border = "2px solid green";
+    console.log("true");
+    return true;
+  }
+}
+
+function checkall() {
+  if (validateName() && validatePhone() && validateEmail()) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 // Header
 // Scroll_big
